9a8e62f0540fa47331fb313208e6048e058353e7
tjoudi <tholfikar.joudi@gmail.com>
2021-06-20 08:49:46 +0000

    fix manifest file

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -1,20 +1,4 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'Odoo Academy',
-    'summary': """Academy app to manage training""",
-    'description': """
-                Academy module to manage Training:
-                - Courses
-                - Sessions
-                - Attendees
-    """,
-    'author': 'tdj',
-    'website' 'google.com',
-    'category' : 'Training',
-    'version' : '0.1',
-    'depends' : ['base'],
-    'data' : [],
-    'demo': [],
-    
-    
-    
+    'name': 'Odoo Academy',     
 }
