cd11e72dfd85f48f1bf205123c2451c36e418121
tjoudi <tholfikar.joudi@gmail.com>
2021-06-20 07:39:18 +0000

    initial commit

---
+++ odoo_academy/.ipynb_checkpoints/__mainfest__-checkpoint.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage training""",
+    'description': """
+                Academy module to manage Training:
+                - Courses
+                - Sessions
+                - Attendees
+    """
+    
+}
---
+++ odoo_academy/__mainfest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage training""",
+    'description': """
+                Academy module to manage Training:
+                - Courses
+                - Sessions
+                - Attendees
+    """,
+    'author': 'tdj',
+    'website' 'google.com',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends' : ['base'],
+    'data' : [],
+    'demo': []
+    
+}
